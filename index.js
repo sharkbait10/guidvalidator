d94dcb65f21ffe73902ff4cb1468bc6657e52808
Irene Bonta <ire@foryouandyourcustomers.com>
2017-10-09 17:46:07 +0200

    puppa

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-exports.printMsg = function() {
-  console.log("This is a message from the demo package");
+"use strict";
+function isGuid(stringToTest) {
+    if (stringToTest[0] === "{") {
+    stringToTest = stringToTest.substring(1, stringToTest.length - 1);
+  }
+  var regexGuid = /^(\{){0,1}[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}(\}){0,1}$/gi;
+  return regexGuid.test(stringToTest);
 }
+exports.isGuid = isGuid;
